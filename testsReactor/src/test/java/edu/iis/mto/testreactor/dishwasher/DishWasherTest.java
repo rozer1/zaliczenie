c8404f9eec4bfa28e183a3340a7146846c5386f9
Skorza.Tobiasz <209458@edu.p.lodz.pl>
2020-06-09 18:04:57 +0200

    Second Test dishWasherStartShouldReturnSuccessWithCorrectValuesGiven.

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -8,6 +8,7 @@ import edu.iis.mto.testreactor.dishwasher.engine.Engine;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.hamcrest.Matchers.is;
 
 public class DishWasherTest {
@@ -26,6 +27,23 @@ public class DishWasherTest {
         ProgramConfiguration program = ProgramConfiguration.builder().withProgram(WashingProgram.ECO).withTabletsUsed(true).withFillLevel(FillLevel.HALF).build();
         RunResult result = dishWasher.start(program);
         RunResult expectedResult = RunResult.builder().withStatus(Status.DOOR_OPEN).build();
+        assertEquals(result.getStatus(), expectedResult.getStatus());
+    }
+
+    @Test
+    public void dishWasherStartShouldReturnSuccessWithCorrectValuesGiven() {
+        Door door = mock(Door.class);
+        DirtFilter dirtFilter = mock(DirtFilter.class);
+        Engine engine = mock(Engine.class);
+        WaterPump waterpPump = mock(WaterPump.class);
+
+        when(door.closed()).thenReturn(true);
+        when(dirtFilter.capacity()).thenReturn(33.0d);
+
+        DishWasher dishWasher = new DishWasher(waterpPump, engine, dirtFilter, door);
+        ProgramConfiguration program = ProgramConfiguration.builder().withProgram(WashingProgram.ECO).withTabletsUsed(true).withFillLevel(FillLevel.HALF).build();
+        RunResult result = dishWasher.start(program);
+        RunResult expectedResult = RunResult.builder().withStatus(Status.ERROR_FILTER).build();
         assertEquals(result.getStatus(), expectedResult.getStatus());
     }
 
